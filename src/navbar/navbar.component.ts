7899ff818083523e7d7af24fe480484f2835c246
Olivera2708 <olivera.radovanovic2002@gmail.com>
2024-11-21 17:42:56 +0100

    navbar done

---
+++ src/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgIf, NgClass],
+  templateUrl: './navbar.component.html',
+  standalone: true,
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  navbarOpen: boolean = false;
+  isPhone = false;
+  isInitialized: boolean = false;
+
+  ngOnInit(): void {
+    this.checkIfPhone();
+    this.isInitialized = true;
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkIfPhone();
+  }
+
+  private checkIfPhone() {
+    this.isPhone = window.innerWidth <= 768;
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+}
